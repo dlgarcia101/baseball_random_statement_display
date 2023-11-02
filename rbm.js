281836c7e36bab88736294f11981f639325fa257
dlgarcia101 <48992986+dlgarcia101@users.noreply.github.com>
2023-11-02 12:31:08 -0400

    Update to DOM
    display msg in msg box

--- rbm.js
+++ rbm.js
@@ -13,6 +13,27 @@ const rndSelector = (arr) =>{
 	return rndPick;
 }
 
+let randomMsg = `in ${year[rndSelector(year)]} ${players[rndSelector(players)]} lead the ${league[rndSelector(league)]} league in ${category[rndSelector(category)]}`;
+
+
+// select button & display
+let getMsgBtn = document.querySelector('button')
+let displayElement = document.querySelector('p');
+
+function seeRndMsg(){
+	displayElement.innerHTML = randomMsg;
+  
+  }
+  
+  getMsgBtn.onclick = seeRndMsg;
+ 
+
+  
+
+
+
+
+
 
 console.log(`in ${year[rndSelector(year)]} ${players[rndSelector(players)]} lead the ${league[rndSelector(league)]} league in ${category[rndSelector(category)]}`);
 
