23aa481aa50303770e9eec88a859147280f0aa87
dlgarcia101 <48992986+dlgarcia101@users.noreply.github.com>
2024-04-18 16:10:06 -0400

    Update of HTML file

--- rbm.js
+++ rbm.js
@@ -1,40 +1,28 @@
 
 // random baseball msg program
 
+
+// list of diff arrays for msg
 let players =['Barry Bonds','Jose Canceco','Mark Maquire','Frank Thomas','Alex Rodgriguez'];
 let league = ['national','american'];
 let year = ['1993','2001','1999','2010','2011'];
 let category = ['home runs','steals','rbi','strike outs','walks'];
 
-//'in ${year} ${playersName} lead the ${League} in ${category} ' 
 
+// pick random number for array picks
 const rndSelector = (arr) =>{
 	let rndPick = Math.floor(Math.random()* arr.length);
 	return rndPick;
 }
 
-let randomMsg = `in ${year[rndSelector(year)]} ${players[rndSelector(players)]} lead the ${league[rndSelector(league)]} league in ${category[rndSelector(category)]}`;
-
-
 // select button & display
 let getMsgBtn = document.querySelector('button')
 let displayElement = document.querySelector('p');
 
+
 function seeRndMsg(){
-	displayElement.innerHTML = randomMsg;
+	displayElement.innerHTML = `in ${year[rndSelector(year)]} ${players[rndSelector(players)]} lead the ${league[rndSelector(league)]} league in ${category[rndSelector(category)]}`;
 	getMsgBtn.style.backgroundColor = 'pink';
-  
-  }
-  
-  getMsgBtn.onclick = seeRndMsg;
- 
+}
 
-  
-
-
-
-
-
-
-console.log(`in ${year[rndSelector(year)]} ${players[rndSelector(players)]} lead the ${league[rndSelector(league)]} league in ${category[rndSelector(category)]}`);
-
+  getMsgBtn.addEventListener('click',seeRndMsg);
